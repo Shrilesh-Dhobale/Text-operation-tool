eb3a6f8dd5dd33970f9bfac9e541c5a89efd9051
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-19 15:55:03 +0530

    upperCase fun created

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 const inputElement = document.getElementById('inputText');
 const outputElement = document.getElementById('outputText');
+
+const upperCase = () => {
+    outputElement.innerText = `Output: ${inputElement.value.toUpperCase()}`;
+
+}
